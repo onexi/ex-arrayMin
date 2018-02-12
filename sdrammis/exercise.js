3b160bac327badb0f1c37115c3e0913ee1f1ae77
sdrammis <sdrammis@mit.edu>
2018-02-11 22:01:17 -0500

    Updated exercise file(s)

--- sdrammis/exercise.js
+++ sdrammis/exercise.js
@@ -4,7 +4,7 @@ exercise.min = function(unordered) {
     var min;
 
     // Find the minimum value in an array
-    return Number.min(unordered)
+    return unordered.sort()[0];
 };
 
 var unordered = [11, 2, 15, 4, 5, 20, 7, 17, 13, 10, 1, 12, 9, 14, 3, 16, 8, 18, 19, 6];
