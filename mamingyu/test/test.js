d8cf78bbf74402a2ae60c071d970f29e351b4019
mamingyu <imamingyu@gmail.com>
2018-02-14 14:55:01 +0800

    Updated exercise file(s)

--- mamingyu/test/test.js
+++ mamingyu/test/test.js
@@ -7,7 +7,7 @@ describe('Array Exercise', function() {
    			var list1 = [11,2,15,4,5,20,7,17,13,10,1,12,9,14,3,16,8,18,19,6];
 
             // compare result of 'order' function to sorted list2
-            console.log(ex.min(list1))
+            // console.log(ex.min(list1))
 	        ex.min(list1).should.equal(1);
 	});
 });
