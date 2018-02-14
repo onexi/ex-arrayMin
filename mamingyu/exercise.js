d5bb1d5fa6efcda0afaf9382c511164624ca7ab7
mamingyu <imamingyu@gmail.com>
2018-02-14 14:55:43 +0800

    Updated exercise file(s)

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -8,7 +8,7 @@ exercise.min = function(unordered) {
     // ---------- Your Code ----------
     // -------------------------------	
     // console.log("hello test test with Derek on tuesday night EST");
-    return 1;
+    return 2;
 };
 
 var unordered = [11, 2, 15, 4, 5, 20, 7, 17, 13, 10, 1, 12, 9, 14, 3, 16, 8, 18, 19, 6];
