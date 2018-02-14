57ec8a26ca7a71967a4e9862bead0ba84c49a155
mamingyu <imamingyu@gmail.com>
2018-02-14 13:49:38 +0800

    Updated exercise file(s)

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -8,7 +8,7 @@ exercise.min = function(unordered) {
     // ---------- Your Code ----------
     // -------------------------------	
 
-    return min;
+    return 1;
 };
 
 var unordered = [11, 2, 15, 4, 5, 20, 7, 17, 13, 10, 1, 12, 9, 14, 3, 16, 8, 18, 19, 6];
