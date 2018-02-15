f87fe1334af1a59da8319df39b4da8a5504b355e
mamingyu <imamingyu@gmail.com>
2018-02-16 01:39:39 +0800

    Updated exercise file(s)

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -7,7 +7,7 @@ exercise.min = function(unordered) {
     // -------------------------------	
     // ---------- Your Code ----------
     // -------------------------------	
-    console.log("hello test test with Derek on tuesday night EST");
+    console.log("hello test test with Derek on tuesday night EST 0139");
     return 1;
 };
 
