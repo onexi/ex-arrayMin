93666db3b885850073feef3233b9b380edfcda51
mamingyu <imamingyu@gmail.com>
2018-02-16 10:19:40 +0800

    Updated exercise file(s)

--- mamingyu/exercise.js
+++ mamingyu/exercise.js
@@ -2,12 +2,12 @@ var exercise = {};
 
 exercise.min = function(unordered) {
     var min;
-    console.log("hello test test with Derek on tuesday night EST 10141");
+    console.log("hello test test with Derek on tuesday night EST 10191");
     // Find the minimum value in an array
     // -------------------------------	
     // ---------- Your Code ----------
     // -------------------------------	
-    console.log("hello test test with Derek on tuesday night EST 1014");
+    console.log("hello test test with Derek on tuesday night EST 1019");
     return 1;
 };
 
